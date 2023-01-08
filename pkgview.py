91c9e6e990d9a4954d0085e759225976ea22f23e
drakkar1969 <nicolabay@outlook.com>
2023-01-08 10:43:14 +0100

    Build package list from all databases in app startup

--- pkgview.py
+++ pkgview.py
@@ -10,11 +10,12 @@ import pyalpm, datetime
 #-- CLASS: PKGOBJECT
 #------------------------------------------------------------------------------
 class PkgObject(GObject.Object):
-	pkg = GObject.Property(type=GObject.TYPE_PYOBJECT)
-
 	#-----------------------------------
 	# Properties
 	#-----------------------------------
+	pkg = GObject.Property(type=GObject.TYPE_PYOBJECT, default=None)
+	reason = GObject.Property(type=int, default=-1)
+
 	@GObject.Property(type=str, default="")
 	def name(self):
 		return(self.pkg.name)
@@ -29,16 +30,19 @@ class PkgObject(GObject.Object):
 
 	@GObject.Property(type=str, default="")
 	def status(self):
-		if self.pkg.reason == 0:
+		if self.reason == -1: return("")
+
+		if self.reason == 0:
 			return("installed")
 		else:
-			if self.pkg.compute_requiredby() != []: return("dependency")
-			else:
-				return("optional" if self.pkg.compute_optionalfor() != [] else "orphan")
+			# if self.pkg.compute_requiredby() != []: return("dependency")
+			# else:
+			# 	return("optional" if self.pkg.compute_optionalfor() != [] else "orphan")
+			return("dependency")
 
 	@GObject.Property(type=str, default="")
 	def sdate(self):
-		return(datetime.datetime.fromtimestamp(self.pkg.installdate).strftime("%Y/%m/%d %H:%M"))
+		return(datetime.datetime.fromtimestamp(self.pkg.installdate).strftime("%Y/%m/%d %H:%M") if self.pkg.installdate != 0 else "")
 
 	@GObject.Property(type=int, default=0)
 	def date(self):
@@ -218,7 +222,7 @@ class MainWindow(Adw.ApplicationWindow):
 		self.repo_listbox.select_row(self.repolist_allrow)
 		self.status_listbox.select_row(self.statuslist_installedrow)
 
-		for db in app.db_files:
+		for db in app.db_names:
 			box = Gtk.Box(margin_start=6, margin_end=6, spacing=6)
 			box.append(Gtk.Image(icon_name="extract-archive-symbolic"))
 			box.append(Gtk.Label(label=str.title(db)))
@@ -241,25 +245,34 @@ class LauncherApp(Adw.Application):
 		# Connect signal handlers
 		self.connect("activate", self.on_activate)
 
+		# Get path to pacman databases
 		alpm_folder = "/var/lib/pacman"
 
 		db_path = os.path.join(alpm_folder, "sync")
 
+		# Build list of database names
 		db_files = list(os.listdir(db_path)) if os.path.exists(db_path) else []
-		self.db_files = [os.path.basename(db).split(".")[0] for db in db_files]
+		self.db_names = [os.path.basename(db).split(".")[0] for db in db_files]
 
+		# Get pyalpm handle
 		alpm_handle = pyalpm.Handle("/", alpm_folder)
-		self.local_db = alpm_handle.get_localdb()
 
 		self.pkg_objects = []
 
-		# for db in db_files:
-		# 	sync_db = alpm_handle.register_syncdb(db, pyalpm.SIG_DATABASE_OPTIONAL)
+		# Build dictionary of names,install reasons of local packages
+		local_db = alpm_handle.get_localdb()
+		local_dict = dict([(pkg.name, pkg.reason) for pkg in local_db.pkgcache])
 
-		# 	if sync_db is not None:
-		# 		self.pkg_objects.extend([PkgObject(pkg) for pkg in sync_db.pkgcache])
+		# Build list of PkgOBjects from packages in databases
+		for db in self.db_names:
+			sync_db = alpm_handle.register_syncdb(db, pyalpm.SIG_DATABASE_OPTIONAL)
 
-		self.pkg_objects.extend([PkgObject(pkg) for pkg in self.local_db.pkgcache])
+			if sync_db is not None:
+				self.pkg_objects.extend([PkgObject(pkg) for pkg in sync_db.pkgcache])
+
+		for i, obj in enumerate(self.pkg_objects):
+			if obj.pkg.name in local_dict.keys():
+				self.pkg_objects[i].reason = local_dict[obj.pkg.name]
 
 	#-----------------------------------
 	# Signal handlers
