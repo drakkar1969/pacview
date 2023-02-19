ec56d9ad82af9b14797ee11aee73c43b2dd4b87d
drakkar1969 <nicolabay@outlook.com>
2023-02-19 10:02:25 +0100

    Bump version to 1.0.rc7

--- app/pacview.py
+++ app/pacview.py
@@ -1153,7 +1153,7 @@ class MainWindow(Adw.ApplicationWindow):
 			application_name="PacView",
 			application_icon="software-properties",
 			developer_name="draKKar1969",
-			version="1.0.rc6",
+			version="1.0.rc7",
 			comments="A Pacman database and AUR browser for Arch Linux, heavily inspired by <a href='https://osdn.net/projects/pkgbrowser/'>PkgBrowser</a>",
 			website="https://github.com/drakkar1969/pacview",
 			developers=["draKKar1969"],
