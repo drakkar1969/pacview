7685447cfc6240536592e6bc3b906c0b8d89180b
drakkar1969 <nicolabay@outlook.com>
2024-01-13 16:50:18 +0100

    Bump version to 2.2.0

--- src/app.rs
+++ src/app.rs
@@ -159,7 +159,7 @@ impl PacViewApplication {
             .application_name("PacView")
             .application_icon("software-properties")
             .developer_name("draKKar1969")
-            .version("2.1.4")
+            .version("2.2.0")
             .website("https://github.com/drakkar1969/pacview")
             .developers(vec!["draKKar1969"])
             .designers(vec!["draKKar1969"])
