e889f1c0bc323dc7ebabc572a566003a2726eaa4
drakkar1969 <nicolabay@outlook.com>
2024-02-03 10:59:15 +0100

    Implement copy and open in file manager in backup window

--- src/backup_window.rs
+++ src/backup_window.rs
@@ -5,6 +5,7 @@ use glib::clone;
 
 use crate::pkg_object::PkgObject;
 use crate::backup_object::BackupObject;
+use crate::utils::Utils;
 
 //------------------------------------------------------------------------------
 // MODULE: BackupWindow
@@ -20,11 +21,17 @@ mod imp {
     pub struct BackupWindow {
         #[template_child]
         pub filter_dropdown: TemplateChild<gtk::DropDown>,
+        #[template_child]
+        pub open_button: TemplateChild<gtk::Button>,
+        #[template_child]
+        pub copy_button: TemplateChild<gtk::Button>,
 
         #[template_child]
         pub view: TemplateChild<gtk::ColumnView>,
         #[template_child]
         pub model: TemplateChild<gio::ListStore>,
+        #[template_child]
+        pub selection: TemplateChild<gtk::SingleSelection>,
         #[template_child]
         pub status_filter: TemplateChild<gtk::StringFilter>,
     }
@@ -97,6 +104,7 @@ impl BackupWindow {
     fn setup_signals(&self) {
         let imp = self.imp();
 
+        // Filter dropdown selected item property notify signal
         imp.filter_dropdown.connect_selected_item_notify(clone!(@weak imp => move |dropdown| {
             if let Some(sel) = dropdown.selected_item()
                 .and_downcast::<gtk::StringObject>()
@@ -108,6 +116,31 @@ impl BackupWindow {
                     imp.status_filter.set_search(Some(&sel));
                 }
             }
+        }));
+
+        // Open button clicked signal
+        imp.open_button.connect_clicked(clone!(@weak imp => move |_| {
+            let item = imp.selection.selected_item()
+                .and_downcast::<BackupObject>()
+                .expect("Must be a 'BackupObject'");
+
+            Utils::open_file_manager(&item.filename());
+        }));
+
+        // Copy button clicked signal
+        imp.copy_button.connect_clicked(clone!(@weak self as window, @weak imp => move |_| {
+            let copy_text = imp.selection.iter::<glib::Object>().flatten()
+                .map(|item| {
+                    let backup = item
+                        .downcast::<BackupObject>()
+                        .expect("Must be a 'BackupObject'");
+
+                    format!("{package} => {filename} ({status})", package=backup.package().unwrap_or("None".to_string()), filename=backup.filename(), status=backup.status_text())
+                })
+                .collect::<Vec<String>>()
+                .join("\n");
+
+            window.clipboard().set_text(&copy_text);
         }));
     }
 
