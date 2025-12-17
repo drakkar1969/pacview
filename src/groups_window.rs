bb0b550e113a76e4d81ff73ebc4a05b593dc90e7
drakkar1969 <nicolabay@outlook.com>
2025-12-17 22:25:30 +0100

    Scroll to start in backup/groups window

--- src/groups_window.rs
+++ src/groups_window.rs
@@ -47,6 +47,8 @@ mod imp {
         pub(super) copy_button: TemplateChild<gtk::Button>,
 
         #[template_child]
+        pub(super) scroll_window: TemplateChild<gtk::ScrolledWindow>,
+        #[template_child]
         pub(super) view: TemplateChild<gtk::ListView>,
         #[template_child]
         pub(super) model: TemplateChild<gio::ListStore>,
@@ -394,6 +396,15 @@ impl GroupsWindow {
                 // Populate column view
                 imp.model.splice(0, 0, &pkg_list);
             });
+
+            // Scroll to start
+            glib::idle_add_local_once(clone!(
+                #[weak] imp,
+                move || {
+                    let v_adjust = imp.scroll_window.vadjustment();
+                    v_adjust.set_value(v_adjust.lower());
+                }
+            ));
         }
     }
 }
