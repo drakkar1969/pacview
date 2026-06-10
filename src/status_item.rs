e106aebcb76f9dd0add5e8dced49cdc565f964fc
drakkar1969 <nicolabay@outlook.com>
2026-06-10 18:59:42 +0300

    Change update indicator in sidebar (replace error button with icons)

--- src/status_item.rs
+++ src/status_item.rs
@@ -40,11 +40,8 @@ mod imp {
         #[property(get, set, construct_only)]
         id: Cell<PkgFlags>,
 
-        pub(super) error_button: RefCell<gtk::MenuButton>,
         pub(super) spinner: RefCell<adw::Spinner>,
         pub(super) count_label: RefCell<gtk::Label>,
-
-        pub(super) error_label: RefCell<gtk::Label>,
     }
 
     //---------------------------------------
@@ -91,22 +88,14 @@ impl StatusItem {
         // Store widgets
         let imp = obj.imp();
 
-        let error_button = builder.object::<gtk::MenuButton>("error_button")
-            .expect("Failed to get object from builder");
-
         let spinner = builder.object::<adw::Spinner>("spinner")
             .expect("Failed to get object from builder");
 
         let count_label = builder.object::<gtk::Label>("count_label")
             .expect("Failed to get object from builder");
 
-        let error_label = builder.object::<gtk::Label>("error_label")
-            .expect("Failed to get object from builder");
-
-        imp.error_button.replace(error_button);
         imp.spinner.replace(spinner);
         imp.count_label.replace(count_label);
-        imp.error_label.replace(error_label);
 
         obj
     }
@@ -130,21 +119,30 @@ impl StatusItem {
         let imp = self.imp();
 
         let count_label = imp.count_label.borrow();
-        let error_button = imp.error_button.borrow();
-        let error_label = imp.error_label.borrow();
 
         imp.spinner.borrow().set_visible(state == StatusItemState::Checking);
 
         if let StatusItemState::Updates(count, error) = state {
+            let icon = if error.is_some() {
+                "status-updates-error-symbolic"
+            } else if count == 0 {
+                "status-updates-symbolic"
+            } else {
+                "status-updates-new-symbolic"
+            };
+
+            self.set_icon_name(Some(icon));
+
             count_label.set_visible(count != 0);
             count_label.set_label(&count.to_string());
 
-            error_button.set_visible(error.is_some());
-            error_label.set_label(&error.unwrap_or_default());
+            self.set_tooltip(error.as_deref());
         } else {
+            self.set_icon_name(Some("status-updates-symbolic"));
+
             count_label.set_visible(false);
 
-            error_button.set_visible(false);
+            self.set_tooltip(None);
         }
     }
 }
