6a9d9bfabdd2f38a1ad25504f893d5cb95629c03
drakkar1969 <nicolabay@outlook.com>
2026-08-24 13:05:27 +0300

    Revamp StatsWindow UI design

--- src/stats_window.rs
+++ src/stats_window.rs
@@ -1,5 +1,4 @@
 use std::cell::Cell;
-use std::fmt::Write as _;
 
 use gtk::{glib, gio, gdk};
 use adw::subclass::prelude::*;
@@ -30,11 +29,9 @@ mod imp {
     #[template(resource = "/com/github/PacView/ui/stats_window.ui")]
     pub struct StatsWindow {
         #[template_child]
-        pub(super) view: TemplateChild<gtk::ColumnView>,
+        pub(super) repo_grid: TemplateChild<gtk::Grid>,
         #[template_child]
-        pub(super) model: TemplateChild<gio::ListStore>,
-        #[template_child]
-        pub(super) selection: TemplateChild<gtk::NoSelection>,
+        pub(super) total_grid: TemplateChild<gtk::Grid>,
 
         #[property(get, set)]
         is_loaded: Cell<bool>,
@@ -54,9 +51,6 @@ mod imp {
 
             klass.bind_template();
 
-            // Install actions
-            Self::install_actions(klass);
-
             // Add key bindings
             Self::bind_shortcuts(klass);
         }
@@ -67,57 +61,18 @@ mod imp {
     }
 
     #[glib::derived_properties]
-    impl ObjectImpl for StatsWindow {
-        //---------------------------------------
-        // Constructor
-        //---------------------------------------
-        fn constructed(&self) {
-            self.parent_constructed();
-
-            let obj = self.obj();
-
-            obj.setup_widgets();
-        }
-    }
+    impl ObjectImpl for StatsWindow {}
     impl WidgetImpl for StatsWindow {}
     impl WindowImpl for StatsWindow {}
     impl AdwWindowImpl for StatsWindow {}
 
     impl StatsWindow {
         //---------------------------------------
-        // Install actions
-        //---------------------------------------
-        fn install_actions(klass: &mut <Self as ObjectSubclass>::Class) {
-            // Copy action
-            klass.install_action("stats.copy", None, |window, _, _| {
-                let mut output = String::from("## Package Statistics\n|Repository|Packages|Installed|Explicit|Installed Size|\n|---|---|---|---|---|\n");
-
-                for stat in window.imp().selection.iter::<glib::Object>()
-                    .filter_map(|item| item.ok().and_downcast::<StatsObject>()) {
-                        writeln!(output,
-                            "|{repository}|{packages}|{installed}|{explicit}|{size}|",
-                            repository=stat.repository(),
-                            packages=stat.packages(),
-                            installed=stat.installed(),
-                            explicit=stat.explicit(),
-                            size=stat.size()
-                        )
-                        .unwrap();
-                    }
-
-                window.clipboard().set_text(&output);
-            });
-        }
-
-        //---------------------------------------
         // Bind shortcuts
         //---------------------------------------
         fn bind_shortcuts(klass: &<Self as ObjectSubclass>::Class) {
             // Close window binding
             klass.add_binding_action(Key::Escape, ModifierType::NO_MODIFIER_MASK, "window.close");
-
-            // Copy key binding
-            klass.add_binding_action(Key::C, ModifierType::CONTROL_MASK | ModifierType::SHIFT_MASK, "stats.copy");
         }
     }
 }
@@ -133,93 +88,144 @@ glib::wrapper! {
 
 impl StatsWindow {
     //---------------------------------------
-    // Setup widgets
+    // Grid row helper function
     //---------------------------------------
-    fn setup_widgets(&self) {
-        // Set initial focus on view
-        self.imp().view.grab_focus();
+    fn attach_grid_row(grid: &gtk::Grid, row: i32, has_icon: bool, repo: &str, size: i64, inst: usize, pkgs: usize) {
+        // Attach image
+        if has_icon {
+            let image = gtk::Image::builder()
+                .icon_name("repository-symbolic")
+                .build();
+
+            grid.attach(&image, 0, row, 1, 1);
+        }
+
+        // Attach repo name/size labels
+        let repo_box = gtk::Box::builder()
+            .orientation(gtk::Orientation::Vertical)
+            .spacing(2)
+            .build();
+
+        let repo_label = gtk::Label::builder()
+            .xalign(0.0)
+            .label(if repo == "aur" { repo.to_uppercase() } else { repo.to_title_case() })
+            .css_classes(["heading"])
+            .build();
+
+        let size_label = gtk::Label::builder()
+            .xalign(0.0)
+            .label(Size::from_bytes(size).to_string())
+            .css_classes(["caption-heading", "dimmed"])
+            .build();
+
+        repo_box.append(&repo_label);
+        repo_box.append(&size_label);
+
+        if has_icon {
+            grid.attach(&repo_box, 1, row, 1, 1);
+        } else {
+            grid.attach(&repo_box, 0, row, 2, 1);
+        }
+
+        // Attach progress bar and installed/pkgs labels
+        let progress_box = gtk::Box::builder()
+            .orientation(gtk::Orientation::Vertical)
+            .valign(gtk::Align::End)
+            .margin_start(24)
+            .spacing(6)
+            .build();
+
+        let progress_bar = gtk::ProgressBar::builder()
+            .valign(gtk::Align::Center)
+            .fraction(if pkgs == 0 { 0.0 } else { inst as f64 / pkgs as f64 })
+            .css_classes(["horizontal", "stats"])
+            .build();
+
+        let details_box = gtk::Box::builder()
+            .spacing(32)
+            .build();
+
+        let inst_label = gtk::Label::builder()
+            .xalign(0.0)
+            .label(format!("{inst} installed"))
+            .css_classes(["caption-heading", "dimmed"])
+            .build();
+
+        let pkgs_label = gtk::Label::builder()
+            .hexpand(true)
+            .xalign(1.0)
+            .label(format!("{pkgs} packages"))
+            .css_classes(["caption-heading", "dimmed"])
+            .build();
+
+        details_box.append(&inst_label);
+        details_box.append(&pkgs_label);
+
+        progress_box.append(&progress_bar);
+        progress_box.append(&details_box);
+
+        grid.attach(&progress_box, 2, row, 1, 1);
+
     }
 
     //---------------------------------------
     // Populate window
     //---------------------------------------
     fn populate(&self, repos: Vec<String>, pkg_model: &gio::ListStore) {
-        let imp = self.imp();
-
         glib::spawn_future_local(clone!(
-            #[weak] imp,
+            #[weak(rename_to = window)] self,
             #[weak] pkg_model,
             async move {
+                let imp = window.imp();
+
                 let mut pkgs_total = 0;
                 let mut inst_total = 0;
                 let mut size_total = 0;
-                let mut expl_total = 0;
 
                 let pkg_list: Vec<(String, String, i64)> = pkg_model.iter::<PkgObject>()
                     .flatten()
                     .map(|pkg| (pkg.repository(), pkg.status().to_owned(), pkg.install_size()))
                     .collect();
 
-                // Build stats list per repo
-                let mut stats_items: Vec<StatsObject> = repos.iter()
-                    .map(|repo| {
-                        let map = pkg_list.iter()
-                            .filter(|(repository, _, _)| repository == repo)
-                            .into_group_map_by(|(_, status, _)| status);
+                let mut row = 0;
 
-                        let pkgs: usize = map.values()
-                            .map(Vec::len)
-                            .sum();
+                for repo in repos {
+                    let map = pkg_list.iter()
+                        .filter(|(repository, _, _)| repository == &repo)
+                        .into_group_map_by(|(_, status, _)| status);
 
-                        let (inst, size): (usize, i64) = map.iter()
-                            .filter(|&(&key, _)| !key.is_empty())
-                            .map(|(_, value)| {
-                                let inst = value.len();
-                                let size = value.iter()
-                                    .map(|(_, _, size)| *size)
-                                    .sum();
+                    let pkgs: usize = map.values()
+                        .map(Vec::len)
+                        .sum();
 
-                                (inst, size)
-                            })
-                            .reduce(|(acc_inst, acc_size), (inst, size)| {
-                                (acc_inst + inst, acc_size + size)
-                            })
-                            .unwrap_or_default();
+                    let (inst, size): (usize, i64) = map.iter()
+                        .filter(|&(&key, _)| !key.is_empty())
+                        .map(|(_, value)| {
+                            let inst = value.len();
+                            let size = value.iter()
+                                .map(|(_, _, size)| *size)
+                                .sum();
 
-                        let expl: usize = map.iter()
-                            .filter(|&(&key, _)| key == "explicit")
-                            .map(|(_, value)| value.len())
-                            .sum();
+                            (inst, size)
+                        })
+                        .reduce(|(acc_inst, acc_size), (inst, size)| {
+                            (acc_inst + inst, acc_size + size)
+                        })
+                        .unwrap_or_default();
 
-                        // Update total counts
-                        pkgs_total += pkgs;
-                        inst_total += inst;
-                        size_total += size;
-                        expl_total += expl;
+                    // Update total counts
+                    pkgs_total += pkgs;
+                    inst_total += inst;
+                    size_total += size;
 
-                        // Add repo item to stats view
-                        StatsObject::new(
-                            Some("repository-symbolic"),
-                            &(if repo == "aur" { repo.to_uppercase() } else { repo.to_title_case() }),
-                            &pkgs.to_string(),
-                            &inst.to_string(),
-                            &expl.to_string(),
-                            &Size::from_bytes(size).to_string()
-                        )
-                    })
-                    .collect();
+                    if pkgs > 0 {
+                        Self::attach_grid_row(&imp.repo_grid, row, true, &repo, size, inst, pkgs);
 
-                // Add item with totals to stats view
-                stats_items.push(StatsObject::new(
-                    None,
-                    "<b>Total</b>",
-                    &format!("<b>{pkgs_total}</b>"),
-                    &format!("<b>{inst_total}</b>"),
-                    &format!("<b>{expl_total}</b>"),
-                    &format!("<b>{}</b>", Size::from_bytes(size_total))
-                ));
+                        row += 1;
+                    }
+                }
 
-                imp.model.splice(0, imp.model.n_items(), &stats_items);
+                Self::attach_grid_row(&imp.total_grid, row + 1, false, "Total", size_total, inst_total, pkgs_total);
             }
         ));
     }
@@ -230,8 +236,6 @@ impl StatsWindow {
     pub fn show(&self, repos: &[String], pkg_model: &gio::ListStore) {
         let repos = repos.to_owned();
 
-        self.present();
-
         glib::idle_add_local_once(clone!(
             #[weak(rename_to = window)] self,
             #[weak] pkg_model,
@@ -241,6 +245,8 @@ impl StatsWindow {
 
                     window.set_is_loaded(true);
                 }
+
+                window.present();
             }
         ));
     }
