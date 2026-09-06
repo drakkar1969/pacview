e98c483609111d99091a5e8fb8c2c669d3cba489
drakkar1969 <nicolabay@outlook.com>
2026-09-06 08:18:49 +0200

    Use AdwPreferencesDialog for stats dialog

--- src/stats_dialog.rs
+++ src/stats_dialog.rs
@@ -3,7 +3,6 @@ use std::cell::Cell;
 use gtk::{glib, gio, pango};
 use adw::subclass::prelude::*;
 use adw::prelude::*;
-use glib::clone;
 
 use itertools::Itertools;
 use size::Size;
@@ -28,9 +27,9 @@ mod imp {
     #[template(resource = "/com/github/PacView/ui/stats_dialog.ui")]
     pub struct StatsDialog {
         #[template_child]
-        pub(super) repo_grid: TemplateChild<gtk::Grid>,
+        pub(super) repo_group: TemplateChild<adw::PreferencesGroup>,
         #[template_child]
-        pub(super) total_grid: TemplateChild<gtk::Grid>,
+        pub(super) total_group: TemplateChild<adw::PreferencesGroup>,
 
         #[property(get, set)]
         is_loaded: Cell<bool>,
@@ -43,7 +42,7 @@ mod imp {
     impl ObjectSubclass for StatsDialog {
         const NAME: &'static str = "StatsDialog";
         type Type = super::StatsDialog;
-        type ParentType = adw::Dialog;
+        type ParentType = adw::PreferencesDialog;
 
         fn class_init(klass: &mut Self::Class) {
             StatsObject::ensure_type();
@@ -60,6 +59,7 @@ mod imp {
     impl ObjectImpl for StatsDialog {}
     impl WidgetImpl for StatsDialog {}
     impl AdwDialogImpl for StatsDialog {}
+    impl PreferencesDialogImpl for StatsDialog {}
 }
 
 //------------------------------------------------------------------------------
@@ -67,32 +67,46 @@ mod imp {
 //------------------------------------------------------------------------------
 glib::wrapper! {
     pub struct StatsDialog(ObjectSubclass<imp::StatsDialog>)
-        @extends adw::Dialog, gtk::Widget,
+        @extends adw::PreferencesDialog, adw::Dialog, gtk::Widget,
         @implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget, gtk::ShortcutManager;
 }
 
 impl StatsDialog {
     //---------------------------------------
-    // Grid row helper function
+    // Add row helper function
     //---------------------------------------
-    fn attach_grid_row(grid: &gtk::Grid, row: i32, has_icon: bool, repo: &str, size: i64, inst: usize, pkgs: usize) {
-        // Attach image
-        if has_icon {
+    fn add_row(&self, is_total: bool, repo: &str, size: i64, inst: usize, pkgs: usize) {
+        let imp = self.imp();
+
+        // Create row widget
+        let row_box = gtk::Box::builder()
+            .margin_start(20)
+            .margin_end(20)
+            .margin_top(12)
+            .margin_bottom(12)
+            .spacing(10)
+            .build();
+
+        // Append image
+        if !is_total {
             let image = gtk::Image::builder()
                 .icon_name("repository-symbolic")
                 .build();
 
-            grid.attach(&image, 0, row, 1, 1);
+            row_box.append(&image);
         }
 
-        // Attach repo name/size labels
+        // Append repo name/size labels
         let repo_box = gtk::Box::builder()
             .orientation(gtk::Orientation::Vertical)
+            .valign(gtk::Align::End)
             .spacing(2)
             .build();
 
         let repo_label = gtk::Label::builder()
             .xalign(0.0)
+            .max_width_chars(12)
+            .ellipsize(pango::EllipsizeMode::End)
             .label(if repo == "aur" { repo.to_uppercase() } else { repo.to_title_case() })
             .css_classes(["heading"])
             .build();
@@ -106,14 +120,10 @@ impl StatsDialog {
         repo_box.append(&repo_label);
         repo_box.append(&size_label);
 
-        if has_icon {
-            grid.attach(&repo_box, 1, row, 1, 1);
-        } else {
-            grid.attach(&repo_box, 0, row, 2, 1);
-        }
+        row_box.append(&repo_box);
 
-        // Attach progress bar and installed/pkgs labels
-        let progress_box = gtk::Box::builder()
+        // Append progress bar and installed/pkgs labels
+        let info_box = gtk::Box::builder()
             .orientation(gtk::Orientation::Vertical)
             .valign(gtk::Align::End)
             .margin_start(24)
@@ -121,7 +131,6 @@ impl StatsDialog {
             .build();
 
         let progress_bar = gtk::ProgressBar::builder()
-            .valign(gtk::Align::Center)
             .fraction(if pkgs == 0 { 0.0 } else { inst as f64 / pkgs as f64 })
             .css_classes(["horizontal", "stats"])
             .build();
@@ -141,103 +150,94 @@ impl StatsDialog {
             .hexpand(true)
             .xalign(1.0)
             .ellipsize(pango::EllipsizeMode::End)
-            .label(format!("{pkgs} packages"))
+            .label(format!("{pkgs} pkgs"))
             .css_classes(["caption-heading", "dimmed"])
             .build();
 
         details_box.append(&inst_label);
         details_box.append(&pkgs_label);
 
-        progress_box.append(&progress_bar);
-        progress_box.append(&details_box);
+        info_box.append(&progress_bar);
+        info_box.append(&details_box);
 
-        grid.attach(&progress_box, 2, row, 1, 1);
+        row_box.append(&info_box);
 
+        // Create action row
+        let row = adw::ActionRow::builder()
+            .build();
+
+        row.set_child(Some(&row_box));
+
+        if is_total {
+            imp.total_group.add(&row);
+        } else {
+            imp.repo_group.add(&row);
+        }
     }
 
     //---------------------------------------
     // Populate dialog
     //---------------------------------------
-    fn populate(&self, repos: Vec<String>, pkg_model: &gio::ListStore) {
-        glib::spawn_future_local(clone!(
-            #[weak(rename_to = dialog)] self,
-            #[weak] pkg_model,
-            async move {
-                let imp = dialog.imp();
+    fn populate(&self, repos: &[String], pkg_model: &gio::ListStore) {
+        let mut pkgs_total = 0;
+        let mut inst_total = 0;
+        let mut size_total = 0;
 
-                let mut pkgs_total = 0;
-                let mut inst_total = 0;
-                let mut size_total = 0;
+        let pkg_list: Vec<(String, String, i64)> = pkg_model.iter::<PkgObject>()
+            .flatten()
+            .map(|pkg| (pkg.repository(), pkg.status().to_owned(), pkg.install_size()))
+            .collect();
 
-                let pkg_list: Vec<(String, String, i64)> = pkg_model.iter::<PkgObject>()
-                    .flatten()
-                    .map(|pkg| (pkg.repository(), pkg.status().to_owned(), pkg.install_size()))
-                    .collect();
+        for repo in repos {
+            let map = pkg_list.iter()
+                .filter(|(repository, _, _)| repository == repo)
+                .into_group_map_by(|(_, status, _)| status);
 
-                let mut row = 0;
+            let pkgs: usize = map.values()
+                .map(Vec::len)
+                .sum();
 
-                for repo in repos {
-                    let map = pkg_list.iter()
-                        .filter(|(repository, _, _)| repository == &repo)
-                        .into_group_map_by(|(_, status, _)| status);
-
-                    let pkgs: usize = map.values()
-                        .map(Vec::len)
+            let (inst, size): (usize, i64) = map.iter()
+                .filter(|&(&key, _)| !key.is_empty())
+                .map(|(_, value)| {
+                    let inst = value.len();
+                    let size = value.iter()
+                        .map(|(_, _, size)| *size)
                         .sum();
 
-                    let (inst, size): (usize, i64) = map.iter()
-                        .filter(|&(&key, _)| !key.is_empty())
-                        .map(|(_, value)| {
-                            let inst = value.len();
-                            let size = value.iter()
-                                .map(|(_, _, size)| *size)
-                                .sum();
+                    (inst, size)
+                })
+                .reduce(|(acc_inst, acc_size), (inst, size)| {
+                    (acc_inst + inst, acc_size + size)
+                })
+                .unwrap_or_default();
 
-                            (inst, size)
-                        })
-                        .reduce(|(acc_inst, acc_size), (inst, size)| {
-                            (acc_inst + inst, acc_size + size)
-                        })
-                        .unwrap_or_default();
+            // Update total counts
+            pkgs_total += pkgs;
+            inst_total += inst;
+            size_total += size;
 
-                    // Update total counts
-                    pkgs_total += pkgs;
-                    inst_total += inst;
-                    size_total += size;
-
-                    if pkgs > 0 {
-                        Self::attach_grid_row(&imp.repo_grid, row, true, &repo, size, inst, pkgs);
-
-                        row += 1;
-                    }
-                }
-
-                Self::attach_grid_row(&imp.total_grid, row + 1, false, "Total", size_total, inst_total, pkgs_total);
+            if pkgs > 0 {
+                self.add_row(false, &repo, size, inst, pkgs);
             }
-        ));
+        }
+
+        self.add_row(true, "Total", size_total, inst_total, pkgs_total);
     }
 
     //---------------------------------------
     // Show dialog
     //---------------------------------------
     pub fn show(&self, parent: Option<&impl IsA<gtk::Widget>>, repos: &[String], pkg_model: &gio::ListStore) {
-        let repos = repos.to_owned();
-
         let parent = parent.map(|widget| widget.clone().upcast());
 
-        glib::idle_add_local_once(clone!(
-            #[weak(rename_to = dialog)] self,
-            #[weak] pkg_model,
-            move || {
-                if !dialog.is_loaded() {
-                    dialog.populate(repos, &pkg_model);
+        if !self.is_loaded() {
+            self.populate(repos, &pkg_model);
 
-                    dialog.set_is_loaded(true);
-                }
+            self.set_is_loaded(true);
+        }
 
-                dialog.present(parent.as_ref());
-            }
-        ));
+        self.present(parent.as_ref());
     }
 }
 
