f3d5c424ad88d3efd1f4b5b06b031eb180bb86dc
drakkar1969 <nicolabay@outlook.com>
2026-09-12 09:37:58 +0200

    Remove unneeded clippy allow directives in main window

--- src/window.rs
+++ src/window.rs
@@ -822,7 +822,6 @@ impl PacViewWindow {
     //---------------------------------------
     // Setup alpm
     //---------------------------------------
-    #[allow(clippy::items_after_statements)]
     fn setup_alpm(&self, first_load: bool) {
         let imp = self.imp();
 
@@ -1331,7 +1330,6 @@ impl PacViewWindow {
     // Setup alpm: get package updates
     //---------------------------------------
     #[allow(clippy::future_not_send)]
-    #[allow(clippy::items_after_statements)]
     async fn get_package_updates(&self) {
         let imp = self.imp();
 
